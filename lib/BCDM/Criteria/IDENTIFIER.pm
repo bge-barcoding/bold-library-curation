1fff7b1ca247874e18b6159d8df734d496effb0f
Ben Price <bwprice@users.noreply.github.com>
2025-05-24 09:16:25 +0100

    Update IDENTIFIER.pm
    fixing this issue: https://github.com/bge-barcoding/bold-library-curation/issues/12

--- lib/BCDM/Criteria/IDENTIFIER.pm
+++ lib/BCDM/Criteria/IDENTIFIER.pm
@@ -3,13 +3,14 @@ use strict;
 use warnings;
 use base 'BCDM::Criteria';
 
-# values we assessed are not taxonomic experts
-my %cbg = (
-    'Kate Perez'     => 1,
-    'Angela Telfer'  => 1,
-    'BOLD' => 1,
-    'BLAST' => 1,
-    'BIN' => 1,
+# values we assessed are not taxonomic experts (case-insensitive partial matches)
+my @cbg_patterns = (
+    'Kate Perez',
+    'Angela Telfer',
+    'BOLD',
+    'BLAST',
+    'BIN',
+    'None',
 );
 
 # this so that we know the criterionid for
@@ -26,20 +27,20 @@ sub _assess {
     my $record = shift;
     my $identifier = $record->identified_by;
 
-    # known entity not considered an expert
-    if ( $cbg{$identifier} ) {
-        return 0, "identified_by: '$identifier'";
-    }
-
     # no identifier given
-    elsif ( not $identifier ) {
+    if ( not $identifier ) {
         return 0, "no identifier named";
     }
 
-    # everything else
-    else {
-        return 1, "identified_by: '$identifier'";
+    # check if identifier contains any of the non-expert patterns (case-insensitive)
+    for my $pattern (@cbg_patterns) {
+        if ( $identifier =~ /\Q$pattern\E/i ) {
+            return 0, "identified_by: '$identifier' (contains '$pattern')";
+        }
     }
+
+    # everything else - assumed to be a taxonomic expert
+    return 1, "identified_by: '$identifier'";
 }
 
 1;
