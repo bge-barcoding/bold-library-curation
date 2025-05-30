93c6eaae1fe7183815ba2a81753878bfc59e600f
Ben Price <bwprice@users.noreply.github.com>
2025-05-30 12:15:10 +0100

    Update rank_bold.sh

--- workflow/rank_bold.sh
+++ workflow/rank_bold.sh
@@ -2,8 +2,8 @@
 #SBATCH --partition=day
 #SBATCH --output=job_curate_bold_%j.out
 #SBATCH --error=job_curate_bold_%j.err
-#SBATCH --mem=24G
-#SBATCH --cpus-per-task=4
+#SBATCH --mem=80G
+#SBATCH --cpus-per-task=10
 #SBATCH --mail-user=example@email.com
 #SBATCH --mail-type=ALL
 
@@ -12,10 +12,13 @@ source activate bold-curation
 # use "clean" when running again but backup the results folder first if you want to keep it
 # I've added the unlock function to the clean because I'm tired of doing this manually when something goes wrong
 
-# snakemake --unlock -s workflow/Snakefile -p -c 4 clean
+# unlock implementation - uncomment to run
+# snakemake --unlock -s workflow/Snakefile -p -c 10 clean
 
-# snakemake -s workflow/Snakefile -p -c 4
+# original implementation - uncomment to run
+# snakemake -s workflow/Snakefile -p -c 10
 
-snakemake -s workflow/Snakefile-faster -p -c 4
+# faster implementation with BAGS grading - uncomment to run
+snakemake -s workflow/Snakefile-even-faster-bags -p -c 10
 
 echo Complete!
